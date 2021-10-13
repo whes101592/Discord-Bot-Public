c76c11b13892a2782b12aa38369fe8fc00f0c02d
whes1015 <44525760+whes1015@users.noreply.github.com>
2021-10-14 01:32:09 +0800

    Update index.js

--- Discord-Bot-Public/index.js
+++ Discord-Bot-Public/index.js
@@ -1,5 +1,4 @@
 //#region 變數
-const translate = require('@vitalets/google-translate-api');
 const { MessageEmbed, GuildMember } = require('discord.js');
 const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
